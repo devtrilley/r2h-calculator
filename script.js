5bb87f80f7a58f722561705e09c0b605972307e2
Tamsir Rilley <tamsirrilley@gmail.com>
2024-11-04 13:32:04 -0500

    Fixed focus-visible and added HTML elements to JS

---
+++ script.js
@@ -0,0 +1,32 @@
+/* ALGORITHM/PSEUDOCODE
+  - We click on a button and it get's added to our string in the display section
+  - When we hit "=" (equal button), we evaluate the expression is display
+  - Last, we display the result
+  - Optional:
+    - Add buttons for "()""
+*/
+
+
+// - - - Bringing HTML Elements to JS
+// Number Buttons
+const zeroBtn = document.querySelector(".zero-btn");
+const oneBtn = document.querySelector(".one-btn");
+const twoBtn = document.querySelector(".two-btn");
+const threeBtn = document.querySelector(".three-btn");
+const fourBtn = document.querySelector(".four-btn");
+const fiveBtn = document.querySelector(".five-btn");
+const sixBtn = document.querySelector(".six-btn");
+const sevenBtn = document.querySelector(".seven-btn");
+const eightBtn = document.querySelector(".eight-btn");
+const nineBtn = document.querySelector(".nine-btn");
+
+// Symbol Buttons
+const clearBtn = document.querySelector(".clear-btn");
+const divideBtn = document.querySelector(".divide-btn");
+const multiplyBtn = document.querySelector(".multiply-btn");
+const minusBtn = document.querySelector(".minus-btn");
+const plusBtn = document.querySelector(".plus-btn");
+const decimalBtn = document.querySelector(".decimal-btn");
+const equalBtn = document.querySelector(".equal-btn");
+
+
